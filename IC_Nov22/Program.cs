f18b6cc1aedf226420df1c4a9db57f9d904fe31c
Teenu Thomas <teenuthomas92@gmail.com>
2022-11-20 02:00:39 +1300

    Create New Time and Material Record

--- IC_Nov22/Program.cs
+++ IC_Nov22/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 IWebDriver driver= new ChromeDriver();
 driver.Manage().Window.Maximize();
@@ -24,5 +25,69 @@ if (helloHariString == "Hello hari!")
 {
     Console.WriteLine("Login Failed");
 }
+
+//click on the admistration menu
+
+IWebElement admistrationMenu = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+admistrationMenu.Click();
+
+//click on Time&Material module from dropdown
+IWebElement timeMaterial = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+timeMaterial.Click();
+
+//click on create New Button
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
+
+//select typecode from the dropdown
+IWebElement typeCode = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+typeCode.Click();
+
+Thread.Sleep(1500);//Hold for 1.5 Seconds
+
+IWebElement time = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+time.Click();
+
+//Enter code in textbox
+IWebElement code = driver.FindElement(By.Name("Code"));
+code.SendKeys("CS010");
+
+//Enter Description
+IWebElement description = driver.FindElement(By.Name("Description"));
+description.SendKeys("Nov2022");
+
+//Enter Price per Unit
+IWebElement pricePerUnit = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+pricePerUnit.SendKeys("2500");
+
+//click on save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+
+Thread.Sleep(1500);//Hold for 1.5 Seconds
+
+//click on last page button
+IWebElement gotoLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+gotoLastPageButton.Click();
+
+//check whether new record created or not
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if (newCode.Text == "CS010")
+{
+    Console.WriteLine("Record created successfully");
+}
+else
+{
+    Console.WriteLine("Record not created successfully");
+}
+
+
+
+
+
+
+
+
 
 
