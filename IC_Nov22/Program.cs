40b9cc94ff0675e43ef021175cc818db8da89a9d
Teenu Thomas <teenuthomas92@gmail.com>
2022-11-19 23:45:06 +1300

    Login function

---
+++ IC_Nov22/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver= new ChromeDriver();
+
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login");
+driver.Manage().Window.Maximize ();
+
+IWebElement usernametextbox = driver.FindElement(By.Id("UserName"));
+usernametextbox.SendKeys("hari");
+
+IWebElement passwordtextbox = driver.FindElement(By.Id("Password"));
+passwordtextbox.SendKeys("123123");
+
+IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+loginbutton.Click();
+
+IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+String helloharistring = hellohari.Text;
+
+if (helloharistring == "Hello hari!")
+{
+    Console.WriteLine("Login Successfully");
+} else
+{
+    Console.WriteLine("Login Failed");
+}
+
+
